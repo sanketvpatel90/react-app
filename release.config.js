a74440bd0b107132f49b1ea67a75b46d29818782
Sanket Patel <sanketvpatel90@gmail.com>
2022-04-30 18:14:50 -0700

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/sanketvpatel90/react-app",
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github'
+  ]
+}
